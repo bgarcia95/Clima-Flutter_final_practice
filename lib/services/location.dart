3ff99659a0b0d13b790181afec852d484170e1e0
bgarcia95 <bgarcia.95@outlook.com>
2019-08-27 16:44:01 -0600

    Location class created.

---
+++ lib/services/location.dart
@@ -0,0 +1,18 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude;
+  double longitude;
+
+  Future<void> getLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+    try {
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
